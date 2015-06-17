60be88a47eb7fb4f039a9f96aaf63f44733e8183
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-06-17 05:15:15 +0200

    Finally a working example (spinning triangle moving towards the horizon, in simpledemo.js)

--- simpledemo.js
+++ simpledemo.js
@@ -8,6 +8,7 @@
 
  * TODO | - Progress bars (eg. for shaders and assets) (?)
  *        - Animation, proper way of dealing with state and time
+ *        - 
 
  * SPEC | -
  *        -
@@ -17,32 +18,47 @@
 
 
 $(document).ready(function() {
+
 	//
 	var canvas  = $('#cvs')[0];
-	console.log(canvas);
 	var context = createContext(canvas);
+
+	var modelview  = mat4.create(); //
+	var projection = mat4.create(); //
+	
+	var vertices = createBuffer(context, [[-1, 0, 0],
+		                                  [ 0, 2, 0],
+		                                  [ 1, 0, 0]].flatten(), 3);
+	var colours  = createBuffer(context, [[ 1, 0, 0, 1],
+		                                  [ 1, 0, 0, 1],
+		                                  [ 1, 1, 0, 1]].flatten(), 4);
+
 	// var program = 
 	shaders.programFromSourceFiles(context, 'vertexshader.txt', 'pixelshader.txt').then(function(program) {
 		context.useProgram(program);
 
+		// This part is specific to the shaders we're using.
 		program.vertexPositionAttribute = context.getAttribLocation(program, 'aVertexPosition');
 		context.enableVertexAttribArray(program.vertexPositionAttribute);
 
 		program.vertexColourAttribute = context.getAttribLocation(program, 'aVertexColor');
 		context.enableVertexAttribArray(program.vertexColourAttribute)
 
-		program.pMatrixUniform = context.getUniformLocation(program, 'uPMatrix');
+		program.pMatrixUniform  = context.getUniformLocation(program, 'uPMatrix');
 		program.mvMatrixUniform = context.getUniformLocation(program, 'uMVMatrix');
 
-		render(context);
+		createRenderer(context, program, modelview, projection, [{'vertices': vertices, 'colours': colours}])();
 	});
+
 });
 
 
 
 function createContext(canvas) {
+
 	//
 	var context;
+
 	try {
 		context = canvas.getContext('experimental-webgl');
 		context.viewportWidth = canvas.width;
@@ -58,37 +74,127 @@ function createContext(canvas) {
 			return undefined;
 		}
 	}
+
 }
 
 
-var colour = [0, 0, 0, 1.0];
-var frame = 0;
 
-function render(context) {
+function createBuffer(context, data, itemsize) {
+	
+	//
+	
+	var buffer = context.createBuffer();
+	buffer.itemsize = itemsize;
+	buffer.size     = data.length / buffer.itemsize;
 
-	/**/
-	frame++;
+	context.bindBuffer(context.ARRAY_BUFFER, buffer);
+	context.bufferData(context.ARRAY_BUFFER, new Float32Array(data), context.STATIC_DRAW);
 
-	// context.clearColor(colour[0], colour[1], colour[2], colour[3])
-	context.clearColor((Math.cos(frame*0.003)+1)*0.5, (Math.cos(frame*0.0005)+1)*0.5, 0.8, 1.0);
+	return buffer;
 
-	/* Clear the screen */
-	console.log('Rendering...');
-	context.viewport(0, 0, context.viewportWidth, context.viewportHeight);
-	context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
+}
 
-	/* Update perspective matrix */
-	// mat4.perspective(45, context.viewportWidth/context.viewportHeight, 0.1, 100.0, pMatrix);
-	// mat4.identity(mvMatrix);
 
-	// context.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-	// context.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 
-	/* Draw the meshes */
-	// for (var i = 0; i < scene.length; i++) {
-		// scene[i].draw(shaderProgram);
-	// };
+function setMatrixUniforms(context, program, modelview, projection) {
+	// Specific to our current shaders
+	context.uniformMatrix4fv(program.pMatrixUniform, false, projection);
+	context.uniformMatrix4fv(program.mvMatrixUniform, false, modelview);
+}
 
-	requestAnimationFrame(function() {render(context);}); // TODO: Move this statement
+
+
+function renderBuffer(context, vertexbuffer, colourbuffer, rotation, translation, modelview, projection, program) {
+
+	//
+	mat4.identity(modelview);
+	mat4.translate(modelview, translation);
+	mat4.rotate(modelview, rotation[0], [1, 0, 0]);
+	mat4.rotate(modelview, rotation[1], [0, 1, 0]);
+	mat4.rotate(modelview, rotation[2], [0, 0, 1]);
+
+	//
+	context.bindBuffer(context.ARRAY_BUFFER, vertexbuffer);
+	context.vertexAttribPointer(program.vertexPositionAttribute, vertexbuffer.itemsize, context.FLOAT, false, 0, 0);
+		
+	context.bindBuffer(context.ARRAY_BUFFER, colourbuffer);
+	context.vertexAttribPointer(program.vertexColourAttribute, colourbuffer.itemsize, context.FLOAT, false, 0, 0);
+	
+	// console.log(context, vertexbuffer, colourbuffer, vertexbuffer.size);
+	setMatrixUniforms(context, program, modelview, projection);
+	context.drawArrays(context.TRIANGLES, 0, vertexbuffer.size);
+
+}
+
+
+
+function animate(dt) {
+
+	//
+
+}
+
+
+
+function configure(context, program) {
+
+	//
+	gl.enable(gl.DEPTH_TEST);
+
+	//
+	// TODO: Rename function
+	// This needs some explaining (related to shader programs)
+	program.vertexPositionAttribute = context.getAttribLocation(program, 'aVertexPosition');
+	context.enableVertexAttribArray(program.vertexPositionAttribute);
+
+	program.vertexColourAttribute = context.getAttribLocation(program, 'aVertexColor');
+	context.enableVertexAttribArray(program.vertexColourAttribute);
+
+	program.pMatrixUniform = context.getUniformLocation(program, 'uPMatrix');
+	program.mvMatrixUniform = context.getUniformLocation(program, 'uMVMatrix');
+
+}
+
+
+
+function createRenderer(context, program, modelviewMatrix, projectionMatrix, buffers) {
+
+	//
+	var frame = 0;
+
+	var render = function() {
+
+		// 
+		console.log('Rendering...');
+
+		/**/
+		frame++;
+
+		context.clearColor(0.0, 0.35, 0.42, 1.0); // This is the colour (rgba) that will be used to 'clear' the colour buffer
+
+		/* Clear the screen */
+		// console.log('Rendering...');
+		context.viewport(0, 0, context.viewportWidth, context.viewportHeight); // Set the extent of the viewport
+		context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);    // Clear the depth and colour buffers
+
+		/* Update perspective matrix */
+		mat4.perspective(45, context.viewportWidth/context.viewportHeight, 0.1, 100.0, projectionMatrix);
+		mat4.identity(modelviewMatrix);
+
+		// context.uniformMatrix4fv(program.pMatrixUniform, false, projectionMatrix); //
+		// context.uniformMatrix4fv(program.mvMatrixUniform, false, modelviewMatrix); //
+
+		/* Draw stuff */
+		for (var i = 0; i < buffers.length; i++) {
+			mat4.identity(modelviewMatrix);
+			renderBuffer(context, buffers[i].vertices, buffers[i].colours, [0, frame/60, 0], [0, 0, -frame/60], modelviewMatrix, projectionMatrix, program);
+		}
+
+		/* Schedule the next frame */
+		requestAnimationFrame(render); // TODO: Move this statement
+
+	};
+
+	return render;
 
 };
