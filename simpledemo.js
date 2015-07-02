8023fcdfef55d5be319eb2f69ca5a8391b1e3f88
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-02 22:06:19 +0200

    added log statement

--- simpledemo.js
+++ simpledemo.js
@@ -114,7 +114,8 @@ function createScene(context) {
 
 	var path = 'https://swiftsnamesake.github.io/data/models/';
 	WaveFront.loadMeshes(context, path + 'king.obj', path).then(function(meshes) {
-		scene.push(new Entity({mass: 1.0, velocity: [0,0,0], acceleration: [0,0,0], angular: [0,0,0], position: [0,0,0], rotation: [0,0,0], mesh: meshes  });
+		console.log('Adding model');
+		scene.push(new Entity({mass: 1.0, velocity: [0,0,0], acceleration: [0,0,0], angular: [0,0,0], position: [2,0,0], rotation: [0,0,0], mesh: meshes  });
 	});
 
 	return scene;
